b7d926254b54a0d21cd2447c773416f87484b8d5
Koos85 <koosjoosten@gmail.com>
2024-09-18 21:50:37 +0200

    snmpclient and snmpquery

--- lib/check/ups.py
+++ lib/check/ups.py
@@ -1,5 +1,6 @@
 from asyncsnmplib.mib.mib_index import MIB_INDEX
 from libprobe.asset import Asset
+from ..snmpclient import get_snmp_client
 from ..snmpquery import snmpquery
 
 QUERIES = (
@@ -29,7 +30,8 @@ async def check_ups(
         asset: Asset,
         asset_config: dict,
         check_config: dict) -> dict:
-    state = await snmpquery(asset, asset_config, check_config, QUERIES)
+    snmp = get_snmp_client(asset, asset_config, check_config)
+    state = await snmpquery(snmp, QUERIES)
     for item in state.get('xupsInputEntry', []):
         if 'xupsInputFrequency' in item:
             item['xupsInputFrequency'] *= 10
